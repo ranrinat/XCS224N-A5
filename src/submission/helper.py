0a8bee9d67b082b3e29d95960429b303a85f0d3c
ranrinat <ranrinat@gmail.com>
2022-04-11 18:43:29 +0300

    part c

--- src/submission/helper.py
+++ src/submission/helper.py
@@ -7,11 +7,15 @@ import random
 random.seed(0)
 
 def initialize_vanilla_model(mconf):
+
     attention_model = None
     ### TODO:
     ### [part c]: Make some model here
 
     ### START CODE HERE
+
+    attention_model = GPT(mconf)
+
     ### END CODE HERE
     return attention_model
 
@@ -61,6 +65,19 @@ def finetune(reading_params_path, finetune_corpus_path, pretrain_dataset, block_
     trainer_obj = None #Trainer object (see trainer.py for more details)
     tconf = None #TrainerConfig object (see trainer.py for more details)
     ### START CODE HERE
+
+    if reading_params_path:
+        pass
+
+    tconf = TrainerConfig(max_epochs=75, batch_size=256, learning_rate=6e-4,
+                          lr_decay=True, warmup_tokens=512 * 20, final_tokens= 200*len(pretrain_dataset)*block_size,
+                          num_workers=4)
+
+    name_dataset = NameDataset(open(finetune_corpus_path, encoding='utf-8').read(),
+                               pretrain_dataset)
+    trainer_obj = Trainer(model, name_dataset, None, tconf)
+
+
     ### END CODE HERE
     return tconf, trainer_obj
 
@@ -95,5 +112,8 @@ def train(model, writing_params_path, trainer_obj):
     ### Note: trainer_obj is of type Trainer (see trainer.py for more details)
 
     ### START CODE HERE
+
+    trainer_obj.train()
+
     ### END CODE HERE
     return
